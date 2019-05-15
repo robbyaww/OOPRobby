5c9f4802b3c7771fec92ebb1eca7d860f7bcf58b
robbyaww <robbyawalul@gmail.com>
2019-05-15 08:21:41 +0700

    This is first commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Robby Awalul Meviansyah Abdillah on 15/05/19.
+//  Copyright © 2019 Robby Awalul Meviansyah Abdillah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        print("This is my first update")
+    }
+
+
+}
+
